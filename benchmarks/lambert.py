3de76c10a4cde1b1d66f418211e47c564eb4211d
Nikita Astrakhantsev <nikita.astronaut@gmail.com>
2018-07-27 11:09:13 +0300

    fixed structure

--- benchmarks/lambert.py
+++ benchmarks/lambert.py
@@ -11,10 +11,7 @@ def time_lambert(lambert):
     r = [12214.83399, 10249.46731, 0.0] * u.km
     tof = 76.0 * u.min
 
-    expected_va = [2.058925, 2.915956, 0.0] * u.km / u.s
-    expected_vb = [-3.451569, 0.910301, 0.0] * u.km / u.s
-
-    va, vb = next(lambert(k, r0, r, tof))
+    next(lambert(k, r0, r, tof))
 
 time_lambert.params = ([vallado.lambert, izzo.lambert])
 time_lambert.param_names = ['lambert']
--- benchmarks/perturbations.py
+++ benchmarks/perturbations.py
@@ -18,49 +18,54 @@ from poliastro.bodies import Earth, Moon, Sun
 from poliastro.twobody import Orbit
 
 
-def time_J2_propagation_Earth():
-    r0 = np.array([-2384.46, 5729.01, 3050.46])  # km
-    v0 = np.array([-7.36138, -2.98997, 1.64354])  # km/s
-    k = Earth.k.to(u.km**3 / u.s**2).value
+class J2_propagation():
+    def setup(self):
+        self.r0 = np.array([-2384.46, 5729.01, 3050.46])  # km
+        self.v0 = np.array([-7.36138, -2.98997, 1.64354])  # km/s
+        self.k = Earth.k.to(u.km**3 / u.s**2).value
 
-    orbit = Orbit.from_vectors(Earth, r0 * u.km, v0 * u.km / u.s)
+        self.orbit = Orbit.from_vectors(Earth, self.r0 * u.km, self.v0 * u.km / u.s)
 
-    tof = (1.0 * u.min).to(u.s).value
-    r, v = cowell(orbit, tof, ad=J2_perturbation, J2=Earth.J2.value, R=Earth.R.to(u.km).value)
-
-
-def time_J3_propagation_Earth():
-    a_ini = 8970.667 * u.km
-    ecc_ini = 0.25 * u.one
-    raan_ini = 1.047 * u.rad
-    nu_ini = 0.0 * u.rad
-    argp_ini = 1.0 * u.rad
-    inc_ini = 0.2618 * u.rad
-
-    k = Earth.k.to(u.km**3 / u.s**2).value
-
-    orbit = Orbit.from_classical(Earth, a_ini, ecc_ini, inc_ini, raan_ini, argp_ini, nu_ini)
-
-    tof = (1.0 * u.min).to(u.s).value
-    r_J2, v_J2 = cowell(orbit, tof, ad=J2_perturbation,
-                        J2=Earth.J2.value, R=Earth.R.to(u.km).value, rtol=1e-8)
+        self.tof = (1.0 * u.min).to(u.s).value
+    def time_J2_propagation(self):
+        cowell(self.orbit, self.tof, ad=J2_perturbation, J2=Earth.J2.value, R=Earth.R.to(u.km).value)
 
 
-def time_atmospheric_drag():
-    R = Earth.R.to(u.km).value
-    k = Earth.k.to(u.km**3 / u.s**2).value
+class J3_propagation():
+    def setup(self):
+        self.a_ini = 8970.667 * u.km
+        self.ecc_ini = 0.25 * u.one
+        self.raan_ini = 1.047 * u.rad
+        self.nu_ini = 0.0 * u.rad
+        self.argp_ini = 1.0 * u.rad
+        self.inc_ini = 0.2618 * u.rad
 
-    # parameters of a circular orbit with h = 250 km (any value would do, but not too small)
-    orbit = Orbit.circular(Earth, 250 * u.km)
+        self.k = Earth.k.to(u.km**3 / u.s**2).value
 
-    # parameters of a body
-    C_D = 2.2  # dimentionless (any value would do)
-    A = ((np.pi / 4.0) * (u.m**2)).to(u.km**2).value  # km^2
-    m = 100  # kg
-    B = C_D * A / m
+        self.orbit = Orbit.from_classical(Earth, self.a_ini, self.ecc_ini, self.inc_ini, self.raan_ini, self.argp_ini, self.nu_ini)
+        self.tof = (1.0 * u.min).to(u.s).value
+        self.a_J2J3 = lambda t0, u_, k_: J2_perturbation(t0, u_, k_, J2=Earth.J2.value, R=Earth.R.to(u.km).value) + \
+                                         J3_perturbation(t0, u_, k_, J3=Earth.J3.value, R=Earth.R.to(u.km).value)
+    def time_J3_propagation(self):
+        cowell(self.orbit, self.tof, ad=self.a_J2J3, rtol=1e-8)
 
-    # parameters of the atmosphere
-    rho0 = Earth.rho0.to(u.kg / u.km**3).value  # kg/km^3
-    H0 = Earth.H0.to(u.km).value
-    tof = 60  # s
-    r, v = cowell(orbit, tof, ad=atmospheric_drag, R=R, C_D=C_D, A=A, m=m, H0=H0, rho0=rho0)
+
+class drag():
+    def setup(self):
+        self.R = Earth.R.to(u.km).value
+        self.k = Earth.k.to(u.km**3 / u.s**2).value
+
+        self.orbit = Orbit.circular(Earth, 250 * u.km)
+
+        # parameters of a body
+        self.C_D = 2.2  # dimentionless (any value would do)
+        self.A = ((np.pi / 4.0) * (u.m**2)).to(u.km**2).value  # km^2
+        self.m = 100  # kg
+        self.B = self.C_D * self.A / self.m
+
+        # parameters of the atmosphere
+        self.rho0 = Earth.rho0.to(u.kg / u.km**3).value  # kg/km^3
+        self.H0 = Earth.H0.to(u.km).value
+        self.tof = 60  # s
+    def time_atmospheric_drag(self):
+        cowell(self.orbit, self.tof, ad=atmospheric_drag, R=self.R, C_D=self.C_D, A=self.A, m=self.m, H0=self.H0, rho0=self.rho0)
